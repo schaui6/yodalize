5947f6778ba002446064076454d26c8a2732065f
schaui6 <sanderfer.chau@gmail.com>
2016-03-17 21:20:58 -0700

    Add TTS

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
 
   $('#show').on('click', displayList);
-  $('#hide').on('click', hideList);	
+  $('#hide').on('click', hideList); 
+  $('.say').on('click', sayText); 
 
   $(window).bind("pageshow", function(event) {
     if(event.originalEvent.persisted) {
@@ -10,12 +11,23 @@ $(document).ready(function() {
   });
 });
 
-var displayList = function(event){	
+var displayList = function(event){  
   event.preventDefault();
   $('#display').show();
 }
 
-var hideList = function(event){	
+var hideList = function(event){ 
   event.preventDefault();
   $('#display').hide();
 }
+
+var sayText = function(event){ 
+  event.preventDefault();
+  var text = $(this).parent().children('.spoken_text').text();
+  var msg = new SpeechSynthesisUtterance(text);
+  window.speechSynthesis.speak(msg);
+  // text = encodeURIComponent(text);
+  // var url = "https://translate.google.com/translate_tts?ie=UTF-8&q=" + text + "&tl=en"
+  // $('audio').attr('src', url).get(0).play();
+}
+
